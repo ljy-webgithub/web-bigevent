b7419b49d2de3b75627f834c73e4713b429eda4a
cxyjy <1358794395@qq.com>
2021-05-30 20:56:21 +0800

    完成了主页功能开发

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,6 +1,26 @@
 // 注意 每次调用 $.get 或 $.post 或ajax 会先调用这个函数
-$.ajaxPrefilter(function(options) {
+$.ajaxPrefilter(function (options) {
     console.log(options.url);
     // 拼接请求的根路径
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+
+    // 统一为有权限接口 设置请求头 headers
+    // 判断 检测 是否包含 /my/ 是的话 才需要加请求头
+    if (options.url.indexOf('/my/') != -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    //全局统一挂载 complete 回调函数
+    options.complete = function(res) {
+        // 在complete 回调函数中 可以使用 res.responseJSON拿到服务器响应回来数据
+        if (res.responseJSON.status === 1 &&
+            res.responseJSON.message === '身份认证失败！') {
+            // 强制清空 token
+            localStorage.removeItem('token')
+            //强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+$(function () {
+    //调用getUserInfo
+    getUserInfo()
+    // 给退出 添加点击事件
+    $('#btnLogout').on('click', function () {
+        //提示用户是否确认退出
+        layer.confirm('确定退出?', { icon: 3, title: '提示' }, function (index) {
+            // 清空本地存储的 token
+            localStorage.removeItem('token')
+            // 重新跳转到登录页面
+            location.href = '/login.html'
+            //关闭询问框
+            layer.close(index);
+        });
+    })
+})
+// 获取用户基本信息 getUserInfo
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.msg('获取用户信息失败')
+            }
+            //调用 renderAvatar 渲染用户头像
+            renderAvatar(res.data)
+        }
+    })
+}
+// 渲染用户头像
+function renderAvatar(user) {
+    // 1 获取用户名称 根据说明文档
+    var name = user.nickname || user.username
+    // 2 设置欢迎文本
+    $('welcome').html('欢迎&nbsp;&nbsp' + name)
+    // 3 按需渲染用户头像
+    if (user.user_pic != null) {
+        // 渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        // 渲染文本头像
+        $('.layui-nav-img').hide()
+        //toUpperCase 转成大写
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
